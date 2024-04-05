bafe4753224f045d7aca741d7572cdf6981d2974
MirMax13 <matskiv2005@gmail.com>
2024-04-05 16:25:49 +0300

    05.04 16:25 Add password list display and delete functionality in script.js and input.ejs

--- public/script.js
+++ public/script.js
@@ -74,3 +74,62 @@ function savePassword() {
                 console.error('Error saving password.', error);
             });
 }
+
+function displayPasswordList(){
+    const passwordList = document.getElementById('passwordList');
+
+    fetch('/passwords')
+        .then(response =>response.json())
+        .then(data => {
+            if (data.length > 0){
+                data.forEach(password => {
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = `<p><a href="#" class="password-link" data-password="${password._id}">${password.usage} - ${password.password}</a> - ${password._id}</p>`;
+                    passwordList.appendChild(listItem);
+                });
+                // Додаємо обробник події для кожного елемента списку паролів
+                const passwordLinks = document.querySelectorAll('.password-link');
+                passwordLinks.forEach(link => {
+                    link.addEventListener('click', function(event) {
+                        event.preventDefault();
+                        const password = this.getAttribute('data-password');
+                        document.getElementById('password_id').value = password;
+                    });
+                });
+            } else{
+                passwordList.innerHTML = '<p>No passwords saved.</p>';
+            }
+        })
+        .catch(error => {
+            console.error('Error getting password list.', error);
+        });
+}
+
+function deletePassword(){
+    const password_id = document.getElementById('password_id').value;
+
+    if (!password_id){
+        alert('No password ID provided.');
+        return;
+    }
+
+    fetch(`/password/${password_id}`, {
+        method: 'DELETE',
+    })
+    .then(response => {
+        if (response.ok) {
+          console.log('Password deleted successfully.');
+          alert('Password deleted successfully.');
+        } else if (response.status === 404) {
+          console.error('Password not found.');
+          alert('Password not found.');
+        } else {
+          console.error('Error deleting password:', response.status);
+          alert('Error deleting password.');
+        }
+      })
+      .catch(error => {
+        console.error('Error deleting password:', error);
+        alert('Error deleting password.');
+      });
+  }
