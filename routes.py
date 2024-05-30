0d1c0f06d1d3084cdd5b0ba0a15fa7c68428f83e
Maxym Matskiv <matskiv2005@gmail.com>
2024-05-30 16:15:40 +0300

    Error handle and fix css dependencies

--- routes.py
+++ routes.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from markupsafe import escape
 from flask import render_template
+from flask import request
+from flask import url_for
 
 app = Flask(__name__)
 
+
+
 @app.get("/")
 def input():
+    url_for('static', filename='style.css')
+    url_for('static', filename='style2.css')
+    url_for('static', filename='script.css')
     return render_template("input.ejs")
 
 @app.get("/passwords")
@@ -18,8 +25,14 @@ def generate():
 
 @app.post("/save")
 def save():
-    return "<p>Saving passwords...</p>"
+    file = request.files['file']
+    file.save(f"uploads/{file.filename}")
 
 @app.delete("/password/<int:password_id>")
 def delete_password(password_id):
     return f"<p>Deleting password with id {escape(password_id)}...</p>"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('input.ejs'), 404
+
